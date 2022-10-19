f18be0aa5d63268d888bc4665df550c7f7116267
Le Minh <leminh@Les-Mac-mini-2.local>
2022-10-19 09:26:28 +0700

    no comment

--- src/view/page/initView/index.js
+++ src/view/page/initView/index.js
@@ -5,7 +5,7 @@ import themedStyles from './styles';
 import {useForm, Controller, useController} from 'react-hook-form';
 
 const InitView = () => {
-  const [styles, theme] = useTheme(themedStyles);
+  const [styles, theme] = useTheme(themedStyles );
   return <View style={styles.container} />;
 };
 
